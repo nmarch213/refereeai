48a3691eecc08b7f5e5074dd44c9b51b6257c802
nmarch213 <n.march213@gmail.com>
2024-10-02 15:58:54 -0700

    remove log

--- src/server/utils/openai.ts
+++ src/server/utils/openai.ts
@@ -1,8 +1,6 @@
 import { createOpenAI } from "@ai-sdk/openai";
 // import { env } from "~/env";
 
-console.log(process.env);
-
 export const openai = createOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
